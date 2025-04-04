1d3d9e0d92ffd7f0ecaf719c7df6857cf0d179a8
cdnss <32522595+cdnss@users.noreply.github.com>
2025-04-04 12:57:55 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { Headers } from "https://deno.land/std@0.168.0/http/mod.ts";
 import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 
 // Ambil variabel lingkungan untuk konfigurasi server
